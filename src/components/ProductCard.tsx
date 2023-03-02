9b61bae710457ca04978f6d447832502178f3306
MelvDouc <melv.douc@gmail.com>
2023-03-02 18:54:48 +0100

    wip

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,16 +9,27 @@ export default function ProductCard({ product }: {
       <section className="product-card__img">
         <img src={product.image} alt={product.title} />
       </section>
-      <section className="product-card__desc">
-        <dl>
-          <dt>Description</dt>
-          <dd>{product.description}</dd>
-          <dt>Price</dt>
-          <dd>{product.price}</dd>
-          <dt>Rating</dt>
-          <dd>{product.rating.rate} ({product.rating.count} votes)</dd>
-        </dl>
+      <section className="product-card__details">
+        <div className="product-card__detail">
+          <p className="product-card__detail-title">Description</p>
+          <p>{shortDescription(product.description)}</p>
+        </div>
+        <div className="product-card__detail">
+          <p className="product-card__detail-title">Price</p>
+          <p>{product.price}</p>
+        </div>
+        <div className="product-card__detail">
+          <p className="product-card__detail-title">Rating</p>
+          <p>{product.rating.rate} ({product.rating.count} votes)</p>
+        </div>
       </section>
     </div>
   );
+}
+
+function shortDescription(desc: string, addDots = false): string {
+  if (desc.length <= 150 - 3)
+    return (addDots) ? `${desc}...` : desc;
+
+  return shortDescription(desc.slice(0, desc.lastIndexOf(" ")), true);
 }
